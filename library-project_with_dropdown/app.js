49bbbe81bdc07c3003dac91287c99231de2c8b25
Mi6u3l <miguelbragagomes@gmail.com>
2020-04-20 21:41:45 +0100

    library

---
+++ library-project_with_dropdown/app.js
@@ -0,0 +1,80 @@
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const hbs          = require('hbs');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+const session      = require('express-session');
+const MongoStore   = require('connect-mongo')(session);
+
+
+hbs.registerHelper('isSelected', ( author, bookAuthor ) => {
+  console.log("author", author);
+  console.log("bookAuthor", bookAuthor);
+  if (author === bookAuthor) {
+    console.log("found it");
+    return 'selected';
+  } else {
+    return '';
+  }
+});
+
+mongoose
+  .connect('mongodb://localhost/library-project', {useNewUrlParser: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+// Express View engine setup
+
+app.use(require('node-sass-middleware')({
+  src:  path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+      
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+
+// default value for title local
+app.locals.title = 'Express-yo - Generated with IronGenerator';
+app.locals.subTitle = 'something';
+
+const authRoutes = require('./routes/auth-routes');
+const index = require('./routes/index');
+app.use('/', index);
+app.use('/', authRoutes);
+
+module.exports = app;
